0d1bb61b4f13c26a02e85e97c4834539275f2c93
Syed Luqmaan <syedluqmaan.dev@gmail.com>
2026-03-18 16:32:19 +0530

    fix: component casing for Vercel Linux

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import HeroSection from '../components/HeroSection'
 import ProblemSection from '../components/Problemsection.jsx'
-import PricingSection from '../components/PricingSection'
 import FAQSection from '../components/Faqsection.jsx'
 import FinalCTASection from '../components/FinalCTASection'
 
@@ -9,10 +8,6 @@ export default function Home() {
     <div className="min-h-screen">
       <HeroSection />
       <ProblemSection />
-
-
-
-      {/* <PricingSection /> */}
       <FAQSection />
       <FinalCTASection />
     </div>
